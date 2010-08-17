cb9a2ad33faa3cace96d789b41f0dc6c389c2a73
Jay Fields <jfields@wud-jfields02.(none)>
2010-08-17 09:17:55 -0400

    add comments

--- expectations_test.clj
+++ expectations_test.clj
@@ -1,16 +1,23 @@
 (ns expectations-test
   (:use clojure.test expectations))
 
+; k/v pair in map. matches subset
 (expect {:foo 1} in (assoc {:bar 1} :foo 1)) 
 
+; key in set
 (expect :foo in #{:foo :bar}) 
 
+; number equality
 (expect 2 (inc 1))
 
+; string equality
 (expect "foo" "foo")
 
+; is the regex in the string
 (expect #"foo" (str "boo" "foo" "ar"))
 
+; does the form throw an expeted exception
 (expect ArithmeticException (/ 12 0))
 
+; verify the type of the result
 (expect String "foo")
