38bf311a6325f2155f7200c617a92288588d04a6
Jay Fields <github@jayfields.com>
2010-08-16 20:23:15 -0400

    split the tests from the runner

---
+++ expectations_test.clj
@@ -0,0 +1,16 @@
+(ns expectations-test
+  (:use clojure.test expectations))
+
+(expect {:foo 1} in (assoc {:bar 1} :foo 1)) 
+
+(expect :foo in #{:foo :bar}) 
+
+(expect 2 (inc 1))
+
+(expect "foo" "foo")
+
+(expect #"foo" (str "boo" "foo" "ar"))
+
+(expect ArithmeticException (/ 12 0))
+
+(expect String "foo")
