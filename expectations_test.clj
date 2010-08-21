92ab49a8a62502752743c72e02588714bda9de35
Jay Fields <github@jayfields.com>
2010-08-21 10:32:25 -0400

    adding readme, fixing failure reporting

--- expectations_test.clj
+++ expectations_test.clj
@@ -2,25 +2,25 @@
   (:use expectations))
 
 ;; number equality
-(expect 2 (inc 0))
+(expect 1 (inc 0))
 
 ;; string equality
-(expect "afoo" (identity "foo"))
+(expect "foo" (identity "foo"))
 
 ; map equality
-(expect {:afoo 1 :bar 2 :car 4} (assoc {} :foo 1 :bar 2 :car 4))
+(expect {:foo 1 :bar 2 :car 4} (assoc {} :foo 1 :bar 2 :car 4))
 
 ;; is the regex in the string
-(expect #"afoo" (str "boo" "foo" "ar"))
+(expect #"foo" (str "boo" "foo" "ar"))
 
 ;; does the form throw an expeted exception
-(expect ArithmeticException (/ 12 12))
+(expect ArithmeticException (/ 12 0))
 
 ;; verify the type of the result
-(expect String 1)
+(expect String "foo")
 
 ;; k/v pair in map. matches subset
-(expect {:foox 1} (in {:foo 1 :cat 4}))
+(expect {:foo 1} (in {:foo 1 :cat 4}))
 
 ;; key in set
-(expect :fooee (in (conj #{:foo :bar} :cat)))
+(expect :foo (in (conj #{:foo :bar} :cat)))
