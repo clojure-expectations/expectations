8838545671e18770e9d892f874d88a4eba08dc80
Jay Fields <github@jayfields.com>
2012-02-28 09:17:30 -0500

    remove publishing code

--- project.clj
+++ project.clj
@@ -1,19 +1,10 @@
-(defproject expectations "1.3.5"
+(defproject expectations "1.3.6"
   :description "testing framework"
   :jar-name "expectations.jar"
   :java-source-path "src/java"
   :source-path "src/clojure"
   :test-path "test/clojure"
-  :dev-dependencies [[lein-expectations "0.0.1"]]
+  :dev-dependencies [[lein-expectations "0.0.1"]
+                     [lein-publishers "1.0.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [junit/junit "4.8.1"]])
-
-(ns leiningen.publish
-  (:require leiningen.jar)
-  (:use clojure.java.shell))
-
-(defn publish [project & args]
-  (leiningen.jar/jar project)
-  (let [response (apply sh "fig" "--publish" (str (:name project) "/" (:version project)) args)]
-    (println "OUT:" (:out response))
-    (println "ERR:" (:err response))))
