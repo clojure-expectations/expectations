fe2b5c855643473c9f9558d43bb98d50a22576b4
Jay Fields <github@jayfields.com>
2012-05-01 10:31:28 -0400

    use joda to freeze time, if you ask for it.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :source-path "src/clojure"
   :test-path "test/clojure"
   :dev-dependencies [[lein-expectations "0.0.1"]
-                     [lein-publishers "1.0.4"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [junit/junit "4.8.1"]
-                 [joda-time/joda-time "2.1"]])
+                     [lein-publishers "1.0.4"]
+                     [joda-time/joda-time "2.1"]
+                     [junit/junit "4.8.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]])
