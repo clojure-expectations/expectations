d10fda1415ca5203e9194b75772fedb153442d74
Jay Fields <github@jayfields.com>
2012-12-28 13:37:28 -0500

    upgrade version

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject expectations "1.4.23"
+(defproject expectations "1.4.24"
   :description "testing framework"
   :jar-name "expectations.jar"
   :java-source-paths ["src/java"]
   :source-paths ["src/clojure"]
   :test-paths ["test/clojure"]
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [erajure/erajure "0.0.3"]]
   :plugins [[lein-expectations "0.0.7"]
             [lein-publishers "1.0.4"]]
   :profiles {:dev {:dependencies [[joda-time/joda-time "2.1"]
-                                  [junit/junit "4.8.1"]
-                                  [erajure/erajure "0.0.3"]]}})
+                                  [junit/junit "4.8.1"]]}})
