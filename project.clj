5a92b31e1c6986ddc9000aeeff60b8976bbdfe48
Jay <jfields@drw.com>
2013-02-04 10:53:57 -0500

    a

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject expectations "1.4.28"
+(defproject expectations "1.4.29"
   :description "testing framework"
   :jar-name "expectations.jar"
   :java-source-paths ["src/java"]
   :source-paths ["src/clojure"]
   :test-paths ["test/clojure"]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [erajure/erajure "0.0.3"]]
+                 [erajure/erajure "0.0.3"]
+                 [junit/junit "4.8.1"]]
   :plugins [[lein-expectations "0.0.7"]
             [lein-publishers "1.0.4"]]
-  :profiles {:dev {:dependencies [[joda-time/joda-time "2.1"]
-                                  [junit/junit "4.8.1"]]}})
+  :profiles {:dev {:dependencies [[joda-time/joda-time "2.1"]]}})
