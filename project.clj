b5b2e69eb9219b38a76e51d4e8c7318300478109
Jay Fields <jay@jayfields.com>
2014-02-18 10:05:36 -0500

    next version, no erajure

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject expectations "2.0.0"
+(defproject expectations "2.0.1"
   :description "testing framework"
   :jar-name "expectations.jar"
   :java-source-paths ["src/java"]
   :source-paths ["src/clojure"]
   :test-paths ["test/clojure"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [erajure/erajure "0.0.3"]
                  [junit/junit "4.8.1"]]
   :plugins [[lein-expectations "0.0.7"]
-            [lein-publishers "1.0.4"]]
+            [lein-publishers "1.0.11"]]
   :profiles {:dev {:dependencies [[joda-time/joda-time "2.1"]]}})
