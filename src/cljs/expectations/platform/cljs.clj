90243d1559f3ae086fb08891fea4d4e6f11eb0ef
Ivan Mikushin <i.mikushin@gmail.com>
2015-02-06 12:14:43 +0500

    fix macros: proper quoting

--- src/cljs/expectations/platform/cljs.clj
+++ src/cljs/expectations/platform/cljs.clj
@@ -2,7 +2,7 @@
   (:require [cljs.analyzer.api]))
 
 (defmacro all-ns* []
-  `'(~@(cljs.analyzer.api/all-ns)))
+  `'~(cljs.analyzer.api/all-ns))
 
 (defmacro ns-interns* [ns]
-  `'(~@(cljs.analyzer.api/ns-interns ns)))
+  `'~(cljs.analyzer.api/ns-interns ns))
