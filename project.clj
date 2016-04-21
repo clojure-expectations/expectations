53e328dd18c2c28e2bc5c9e409512ee6b575e49f
Jay <jay@jayfields.com>
2016-04-21 16:32:48 -0400

    Version 2.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject expectations "2.1.6"
+(defproject expectations "2.1.7-SNAPSHOT"
   :description "testing framework"
   :jar-name "expectations.jar"
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
