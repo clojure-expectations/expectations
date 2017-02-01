e96e2a2736735433386d1c8e0d92e89b447a7772
Sean Corfield <sean@corfield.org>
2017-02-01 10:59:31 -0800

    Cut 2.2.0 Alpha 1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject expectations "2.2.0-SNAPSHOT"
+(defproject expectations "2.2.0-alpha1"
   :description "testing framework"
   :jar-name "expectations.jar"
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
