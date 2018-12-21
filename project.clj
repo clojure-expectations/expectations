3853e292b1632b3d5ba4c565556e9caf88c22206
Sean Corfield <sean@corfield.org>
2018-12-20 16:57:32 -0800

    Prep for 2.1.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject expectations "2.1.10-SNAPSHOT"
+(defproject expectations "2.1.10"
   :description "a minimalist's unit testing framework"
   :jar-name "expectations.jar"
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
