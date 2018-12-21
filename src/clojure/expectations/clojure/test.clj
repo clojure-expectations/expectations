06d5522963437be8fa7427fcca151283ab91af38
Sean Corfield <sean@corfield.org>
2018-12-21 13:43:39 -0800

    First set of features beyond simple equality
    * `more-of` lifted out of expected form
    * Expectations based on: regex, exception, class, and simple predicate

--- src/clojure/expectations/clojure/test.clj
+++ src/clojure/expectations/clojure/test.clj
@@ -7,12 +7,24 @@
   We do not support ClojureScript in clojure.test mode, sorry."
   (:require [clojure.test :as t]))
 
+;; stub functions for :refer compatibility:
+;; these should probably throw since they should never be called
+;; and never appear in macroexpanded code!
+(defn more-of [pattern & matches])
+
 (defmacro expect
   "Temporary version, just to jump start things.
 
+  Things implemented so far:
+  * more-of
+  * simple predicate test
+  * class test
+  * exception test
+  * regex test
+  * simple equality
+
   Things to implement:
   * more
-  * more-of
   * more->
   * from-each
   * in
@@ -21,7 +33,30 @@
   * freeze-time
   * context / in-context ?"
   ([a] `(clojure.test/is ~a))
-  ([e a] `(clojure.test/is (~'= ~e ~a))))
+  ([e a]
+   (cond (and (sequential? e) (= 'more-of (first e)))
+         (let [es (mapv (fn [[e a]] `(expect ~e ~a))
+                        (interleave (partition 2 (rest (rest e)))))]
+           `(let [~(second e) ~a] ~@es))
+
+         (and (symbol? e) (resolve e))
+         (let [t (resolve e)]
+           (println e '=> t)
+           (if (= Class (class t))
+             (if (instance? Throwable t)
+               `(clojure.test/is (~'thrown? ~e ~a))
+               `(clojure.test/is (~'instance? ~e ~a)))
+             `(clojure.test/is (~e ~a))))
+
+         (isa? (type e) java.util.regex.Pattern)
+         `(clojure.test/is (re-find ~e ~a))
+
+         :else
+         `(clojure.test/is (~'= ~e ~a)))))
+
+(comment
+  (macroexpand '(expect (more-of a 2 a) 4))
+  (macroexpand '(expect (more-of {:keys [a b c]} 1 a 2 b 3 c) {:a 1 :b 2 :c 3})))
 
 (defn- contains-expect?
   "Given a form, return true if it contains any calls to the 'expect' macro."
@@ -60,7 +95,10 @@
   return the same result when applied to a value. May optionally accept a
   'difference' function that should accept the result of each function and
   return a string explaininhg how they actually differ.
-  For explaining strings, you could use expectations/strings-difference."
+  For explaining strings, you could use expectations/strings-difference.
+  (only when I port it across!)
+
+  Right now this produces pretty awful failure messages. FIXME!"
   ([expected-fn actual-fn]
    (functionally expected-fn actual-fn (constantly "not functionally equivalent")))
   ([expected-fn actual-fn difference-fn]
